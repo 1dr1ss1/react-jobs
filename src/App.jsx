51696b3f5bcb44a3739db7f052686b63273681ea
Mehdi <m.idrissi33@hotmail.com>
2025-06-28 16:14:43 -0400

    Initial commit: React project setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// used this shortcut rafce for arrow functional compotent export default
+import React from "react";
+
+const App = () => {
+  return <div className="bg-blue-500">APP</div>;
+};
+
+export default App;
